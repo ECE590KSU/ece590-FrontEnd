fc0d97ce0bcc02e7b23c8f70543819982709e2e4
tylervanover <yeti.jones@gmail.com>
2015-04-23 22:34:02 -0500

    Drawing state added to FreeDraw for frmFreeDrawController

--- CubeMasterGUI/CubeMasterGUI/FreeDraw.cs
+++ CubeMasterGUI/CubeMasterGUI/FreeDraw.cs
@@ -10,9 +10,13 @@ namespace CubeMasterGUI
         private CubeController.Cube _cube;
         public CubeController.Cube.AXIS SelectedAxis { get; set; }
 
+        public enum DRAWING_MODE { SINGLE, LINE, RECTANGLE, SPHERE };
+        public DRAWING_MODE CurrentDrawingMode { get; set; }
+
         public FreeDraw(ref CubeController.Cube cube)
         {
             _cube = cube;
+            this.CurrentDrawingMode = DRAWING_MODE.SINGLE;
         }
 
 
