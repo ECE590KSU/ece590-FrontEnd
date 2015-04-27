cc7eafaa65404aa2e85d8f01478c5eb2e4a93dfc
Kevin Kleine <kkleine4@gmail.com>
2015-04-27 18:45:41 -0500

    Added first bits of code to render snake on form. NON-WORKING STATE

---
+++ CubeMasterGUI/CubeMasterGUI/Snake.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubeMasterGUI
+{
+    public class Snake
+    {
+        private CubeController.Cube _cube;
+
+        private Hashtable _keys;
+
+        int _x;
+
+        int _y;
+                
+        public Snake(ref CubeController.Cube cube)
+        {
+            _cube = cube;
+            _keys = new Hashtable();
+            _x = 250;
+            _x = 250;
+        }
+
+        public void GameTickHandler()
+        {
+            if (IsPressed(Keys.Right))
+                _x += 4;
+            if (IsPressed(Keys.Left))
+                _x -= 4;
+            if (IsPressed(Keys.Up))
+                _y-= 4;
+            if (IsPressed(Keys.Down))
+                _y += 4;
+        }
+
+        public int GetX()
+        {
+            return _x;
+        }
+
+        public int GetY()
+        {
+            return _y;
+        }
+
+        public void ChangeInputState(Keys key, bool state)
+        {
+            _keys[key] = state;
+        }
+
+        public bool IsPressed(Keys key)
+        {
+            if (_keys[key] == null)
+            {
+                return false;
+            }
+            return (bool)_keys[key];
+        }
+
+
+    }
+
+    internal class SnakeSection
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public SnakeSection()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
--- CubeMasterGUI/CubeMasterGUI/frmSnake.cs
+++ CubeMasterGUI/CubeMasterGUI/frmSnake.cs
@@ -12,16 +12,27 @@ namespace CubeMasterGUI
 {
     public partial class frmSnake : Form
     {
-        private FreeDraw _freeDrawController;
+        private Snake _snakeController;
 
         public frmSnake(ref CubeController.Cube cube, int parentWidth, int parentHeight)
         {
             InitializeComponent();
 
+            _snakeController = new Snake(ref cube);
+
             this.Width = parentWidth;
             this.Height = parentHeight;
 
             InvokeTimerProtocol();
+
+            gameTimer.Interval = 1000 / 60; //roughly 60 fps
+            gameTimer.Tick += new EventHandler(RefreshScreen);
+            gameTimer.Start();
+
+            Graphics canvas = picbxGameSpace.CreateGraphics();
+            Rectangle box = new Rectangle(_snakeController.GetX(), _snakeController.GetY(), 16, 16);
+            canvas.FillRectangle(Brushes.SteelBlue, new Rectangle());
+
         }
 
         private void InvokeTimerProtocol()
@@ -41,5 +52,29 @@ namespace CubeMasterGUI
         {
             this.tmrSnake.ResetTimers();
         }
+
+        private void frmSnake_KeyDown(object sender, KeyEventArgs e)
+        {
+            _snakeController.ChangeInputState(e.KeyCode, true);
+        }
+
+        private void frmSnake_KeyUp(object sender, KeyEventArgs e)
+        {
+            _snakeController.ChangeInputState(e.KeyCode, false);
+        }
+
+        private void RefreshScreen(object sender, EventArgs e)
+        {
+            _snakeController.GameTickHandler();
+            this.picbxGameSpace.Invalidate();
+        }
+
+        private void picbxGameSpace_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+            Rectangle box = new Rectangle(_snakeController.GetX(), _snakeController.GetY(), 16, 16);
+            canvas.FillRectangle(Brushes.SteelBlue, new Rectangle());
+        }
     }
+
 }
