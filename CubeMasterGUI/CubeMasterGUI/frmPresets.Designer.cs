0d3ff8129935e3b2630355076d14a5205a7c4e73
tylervanover <yeti.jones@gmail.com>
2015-05-03 19:51:05 -0500

    Basic panel laid out for form

--- CubeMasterGUI/CubeMasterGUI/frmPresets.Designer.cs
+++ CubeMasterGUI/CubeMasterGUI/frmPresets.Designer.cs
@@ -28,7 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlPresetLauncher = new System.Windows.Forms.Panel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.btnClose = new CubeMasterGUI.btnCloseWindow();
+            this.pnlPresetLauncher.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlPresetLauncher
+            // 
+            this.pnlPresetLauncher.Controls.Add(this.vScrollBar1);
+            this.pnlPresetLauncher.Location = new System.Drawing.Point(13, 13);
+            this.pnlPresetLauncher.Name = "pnlPresetLauncher";
+            this.pnlPresetLauncher.Size = new System.Drawing.Size(949, 636);
+            this.pnlPresetLauncher.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(932, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 636);
+            this.vScrollBar1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnClose.Location = new System.Drawing.Point(1324, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 30);
+            this.btnClose.TabIndex = 1;
             // 
             // frmPresets
             // 
@@ -36,14 +63,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
             this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pnlPresetLauncher);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPresets";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "frmPresets";
+            this.pnlPresetLauncher.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlPresetLauncher;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private btnCloseWindow btnClose;
     }
 }
